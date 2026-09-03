0dc64261b236b880e440f071ff1c533f6729f909
Kasey <kaseyho.work@gmail.com>
2026-09-03 20:32:32 +0800

    Set up JavaFX application launcher

---
+++ src/main/java/verity/gui/Launcher.java
@@ -0,0 +1,18 @@
+package verity.gui;
+
+import javafx.application.Application;
+
+/**
+ * Launches the Verity JavaFX application.
+ */
+public class Launcher {
+
+    /**
+     * Starts the JavaFX application.
+     *
+     * @param args Command-line arguments passed to JavaFX.
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
